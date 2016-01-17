2e429a64551f8c6cb54bb1be732c5122bd925d44
Pierre Rochard <pierre@rochard.org>
2016-01-16 22:34:58 -0500

    refactoring models

--- cbtools/models.py
+++ cbtools/models.py
@@ -2,20 +2,224 @@ import argparse
 import logging
 import traceback
 
-from sqlalchemy import (create_engine, func, UniqueConstraint, Boolean,
-                        Column, DateTime, Integer, Numeric, String, ForeignKey)
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy import create_engine, func, UniqueConstraint, Boolean
+from sqlalchemy import Column, DateTime, Integer, Numeric, String, ForeignKey
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
 from config import URI
-
 
 engine = create_engine(URI)
 
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = session.query_property()
+
+
+class Accounts(Base):
+    __tablename__ = 'accounts'
+    __table_args__ = {"schema": "cbtools"}
+
+    balance_amount = Column(Numeric)
+    balance_currency = Column(String)
+    created_at = Column(DateTime(timezone=True))
+    currency = Column(String)
+    id = Column(String, primary_key=True)
+    name = Column(String)
+    native_balance_amount = Column(Numeric)
+    native_balance_currency = Column(String)
+    primary = Column(Boolean)
+    resource = Column(String)
+    resource_path = Column(String)
+    type = Column(String)
+    updated_at = Column(DateTime(timezone=True))
+
+    hold = Column(Numeric)
+    hold_currency = Column(String)
+    available = Column(Numeric)
+    available_currency = Column(String)
+    profile_id = Column(String)
+    user_id = Column(String, ForeignKey('cbtools.users.id'))
+    user = relationship("Users", backref=backref('accounts', order_by=id))
+
+
+class Addresses(Base):
+    __tablename__ = 'addresses'
+    __table_args__ = {"schema": "cbtools"}
+
+    account = relationship('Accounts', backref=backref('addresses', order_by=id))
+    account_id = Column(String, ForeignKey('cbtools.accounts.id'))
+    address = Column(String)
+    callback_url = Column(String)
+    created_at = Column(DateTime(timezone=True))
+    id = Column(String, primary_key=True)
+    name = Column(String)
+    resource = Column(String)
+    resource_path = Column(String)
+    updated_at = Column(DateTime(timezone=True))
+
+
+class Exchanges(Base):
+    __tablename__ = 'exchanges'
+    __table_args__ = {"schema": "cbtools"}
+
+    account = relationship('Accounts', backref=backref('addresses', order_by=id))
+    account_id = Column(String, ForeignKey('cbtools.accounts.id'))
+    amount = Column(Numeric)
+    amount_currency = Column(String)
+    committed = Column(Boolean)
+    created_at = Column(DateTime(timezone=True))
+    id = Column(String, primary_key=True)
+    instant = Column(Boolean)
+    payment_method = relationship('PaymentMethods', backref=backref('exchanges', order_by=id))
+    payment_method_id = Column(String, ForeignKey('cbtools.payment_methods.id'))
+    payment_method_resource = Column(String)
+    payment_method_resource_path = Column(String)
+    payout_at = Column(DateTime(timezone=True))
+    resource = Column(String)
+    resource_path = Column(String)
+    status = Column(String)
+    subtotal = Column(Numeric)
+    subtotal_currency = Column(String)
+    total = Column(Numeric)
+    total_currency = Column(String)
+    transaction = Column(String)
+    transaction_id = Column(String)
+    transaction_resource = Column(String)
+    transaction_resource_path = Column(String)
+    updated_at = Column(DateTime(timezone=True))
+
+
+class Fees(Base):
+    __tablename__ = 'fees'
+    __table_args__ = (UniqueConstraint('source_id', 'fee_type', name='fees_unique_constraint'),
+                      {"schema": "cbtools"})
+
+    id = Column(Integer, primary_key=True)
+    source_id = Column(String, ForeignKey('cbtools.exchanges.id'))
+    exchange = relationship('Exchanges', backref=backref('fees', order_by=id))
+    fee_type = Column(String)
+    amount = Column(Numeric)
+    currency = Column(String)
+
+
+class Limits(Base):
+    __tablename__ = 'limits'
+    __table_args__ = (UniqueConstraint('payment_method_id', 'type', 'period_in_days',
+                                       name='limits_unique_constraint'),
+                      {"schema": "cbtools"})
+    id = Column(Integer, primary_key=True)
+
+    payment_method = relationship('PaymentMethods', backref=backref('limits', order_by=id))
+    payment_method_id = Column(String, ForeignKey('cbtools.payment_methods.id'))
+    period_in_days = Column(Integer)
+    remaining = Column(Numeric)
+    remaining_currency = Column(String)
+    resource = Column(String)
+    total = Column(Numeric)
+    total_currency = Column(String)
+    type = Column(String)
+
+
+class PaymentMethods(Base):
+    __tablename__ = 'payment_methods'
+    __table_args__ = {"schema": "cbtools"}
+
+    allow_buy = Column(Boolean)
+    allow_deposit = Column(Boolean)
+    allow_sell = Column(Boolean)
+    allow_withdraw = Column(Boolean)
+    created_at = Column(DateTime(timezone=True))
+    currency = Column(String)
+    fiat_account = relationship('Accounts', backref=backref('payment_methods', order_by=id))
+    fiat_account_id = Column(String, ForeignKey('cbtools.accounts.id'))
+    fiat_account_resource = Column(String)
+    fiat_account_resource_path = Column(String)
+    id = Column(String, primary_key=True)
+    name = Column(String)
+    primary_buy = Column(Boolean)
+    primary_sell = Column(Boolean)
+    resource = Column(String)
+    resource_path = Column(String)
+    type = Column(String)
+    updated_at = Column(DateTime(timezone=True))
+
+
+class Transactions(Base):
+    __tablename__ = 'transactions'
+    __table_args__ = {"schema": "cbtools"}
+
+    account = relationship('Accounts', backref=backref('addresses', order_by=id))
+    account_id = Column(String, ForeignKey('cbtools.accounts.id'))
+    address_id = Column(String, ForeignKey('cbtools.addresses.id'))
+    address_resource = Column(String)
+    address_resource_path = Column(String)
+    amount = Column(Numeric)
+    amount_currency = Column(String)
+    application_id = Column(String)
+    application_resource = Column(String)
+    buy = relationship('Exchanges', uselist=False, backref=backref('transaction', order_by=id))
+    buy_id = Column(String, ForeignKey('cbtools.exchanges.id'))
+    buy_resource = Column(String)
+    buy_resource_path = Column(String)
+    created_at = Column(DateTime(timezone=True))
+    description = Column(String)
+    fiat_deposit_id = Column(String)
+    fiat_deposit_resource = Column(String)
+    fiat_deposit_resource_path = Column(String)
+    fiat_withdrawal_id = Column(String)
+    fiat_withdrawal_resource = Column(String)
+    fiat_withdrawal_resource_path = Column(String)
+    from_id = Column(String)
+    from_resource = Column(String)
+    from_resource_path = Column(String)
+    id = Column(String, primary_key=True)
+    idem = Column(String)
+    instant_exchange = Column(Boolean)
+    native_amount = Column(Numeric)
+    native_amount_currency = Column(String)
+    network_hash = Column(String)
+    network_status = Column(String)
+    order_id = Column(String)
+    order_resource = Column(String)
+    order_resource_path = Column(String)
+    resource = Column(String)
+    resource_path = Column(String)
+    sell_id = Column(String)
+    sell_resource = Column(String)
+    sell_resource_path = Column(String)
+    status = Column(String)
+    to_address = Column(String)
+    to_email = Column(String)
+    to_id = Column(String)
+    to_resource = Column(String)
+    to_resource_path = Column(String)
+    type = Column(String)
+    updated_at = Column(DateTime(timezone=True))
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    __table_args__ = {"schema": "cbtools"}
+
+    avatar_url = Column(String)
+    bitcoin_unit = Column(String)
+    country_code = Column(String)
+    country_name = Column(String)
+    created_at = Column(DateTime(timezone=True))
+    email = Column(String)
+    id = Column(String, primary_key=True)
+    name = Column(String)
+    native_currency = Column(String)
+    profile_bio = Column(String)
+    profile_location = Column(String)
+    profile_url = Column(String)
+    resource = Column(String)
+    resource_path = Column(String)
+    time_zone = Column(String)
+    username = Column(String)
+
+    # TODO add show authorization information
 
 
 class ReconciliationExceptions(Base):
@@ -36,168 +240,6 @@ class ReconciliationExceptions(Base):
     timestamp = Column(DateTime(timezone=True), default=func.now())
     resolved_timestamp = Column(DateTime(timezone=True))
 
-
-class Users(Base):
-    __tablename__ = 'users'
-    __table_args__ = {"schema": "cbtools"}
-
-    id = Column(String, primary_key=True)
-    name = Column(String)
-    username = Column(String)
-    profile_location = Column(String)
-    profile_bio = Column(String)
-    profile_url = Column(String)
-    avatar_url = Column(String)
-    time_zone = Column(String)
-    native_currency = Column(String)
-    bitcoin_unit = Column(String)
-    country_code = Column(String)
-    country_name = Column(String)
-    created_at = Column(DateTime(timezone=True))
-    email = Column(String)
-
-    document = Column(JSONB)
-
-    #TODO add show authorization information
-
-
-class Accounts(Base):
-    __tablename__ = 'accounts'
-    __table_args__ = {"schema": "cbtools"}
-
-    id = Column(String, primary_key=True)
-    name = Column(String)
-    primary = Column(Boolean)
-    account_type = Column(String)
-    currency = Column(String)
-    balance = Column(Numeric)
-    balance_currency = Column(String)
-    native_balance = Column(Numeric)
-    native_balance_currency = Column(String)
-    hold = Column(Numeric)
-    hold_currency = Column(String)
-    available = Column(Numeric)
-    available_currency = Column(String)
-    created_at = Column(DateTime(timezone=True))
-    updated_at = Column(DateTime(timezone=True))
-    profile_id = Column(String)
-    user_id = Column(String, ForeignKey('cbtools.users.id'))
-    user = relationship("Users", backref=backref('accounts', order_by=id))
-
-    document = Column(JSONB)
-
-
-class Addresses(Base):
-    __tablename__ = 'addresses'
-    __table_args__ = {"schema": "cbtools"}
-
-    id = Column(String, primary_key=True)
-    account_id = Column(String, ForeignKey('cbtools.accounts.id'))
-    account = relationship('Accounts', backref=backref('addresses', order_by=id))
-    address = Column(String)
-    name = Column(String)
-    created_at = Column(DateTime(timezone=True))
-    updated_at = Column(DateTime(timezone=True))
-
-    document = Column(JSONB)
-
-
-class Transactions(Base):
-    __tablename__ = 'transactions'
-    __table_args__ = {"schema": "cbtools"}
-
-    id = Column(String, primary_key=True)
-    transaction_type = Column(String)
-    status = Column(String)
-    amount = Column(Numeric)
-    amount_currency = Column(String)
-    native_amount = Column(Numeric)
-    native_amount_currency = Column(String)
-    description = Column(String)
-    instant_exchange = Column(Boolean)
-    created_at = Column(DateTime(timezone=True))
-    updated_at = Column(DateTime(timezone=True))
-    network_status = Column(String)
-    network_hash = Column(String)
-    to_resource = Column(String)
-    to_address = Column(String)
-    to_user_id = Column(String, ForeignKey('cbtools.users.id'))
-    to_user = relationship('Users', backref=backref('transactions_sent', order_by=id),
-                           foreign_keys=[to_user_id])
-    to_email = Column(String)
-    from_resource = Column(String)
-    from_address = Column(String)
-    from_user_id = Column(String, ForeignKey('cbtools.users.id'))
-    from_user = relationship('Users', backref=backref('transactions_received', order_by=id),
-                             foreign_keys=[from_user_id])
-    address = Column(String, ForeignKey('cbtools.addresses.id'))
-    application_id = Column(String)
-    order_id = Column(String)
-
-    exchange_id = Column(String, ForeignKey('cbtools.exchanges.id'))
-    exchange = relationship('Exchanges', uselist=False, backref=backref('transaction', order_by=id))
-
-    document = Column(JSONB)
-
-
-class Exchanges(Base):
-    __tablename__ = 'exchanges'
-    __table_args__ = {"schema": "cbtools"}
-
-    id = Column(String, primary_key=True)
-    exchange_type = Column(String)
-    status = Column(String)
-    payment_method_id = Column(String, ForeignKey('cbtools.payment_methods.id'))
-    payment_method = relationship('PaymentMethods', backref=backref('exchanges', order_by=id))
-    transaction_id = Column(String)
-    amount = Column(Numeric)
-    amount_currency = Column(String)
-    total = Column(Numeric)
-    total_currency = Column(String)
-    subtotal = Column(Numeric)
-    subtotal_currency = Column(String)
-    created_at = Column(DateTime(timezone=True))
-    updated_at = Column(DateTime(timezone=True))
-    committed = Column(Boolean)
-    instant = Column(Boolean)
-    payout_at = Column(DateTime(timezone=True))
-
-    document = Column(JSONB)
-
-
-class Fees(Base):
-    __tablename__ = 'fees'
-    __table_args__ = (UniqueConstraint('source_id', 'fee_type', name='fees_unique_constraint'),
-                      {"schema": "cbtools"})
-
-    id = Column(Integer, primary_key=True)
-    source_id = Column(String, ForeignKey('cbtools.exchanges.id'))
-    exchange = relationship('Exchanges', backref=backref('fees', order_by=id))
-    fee_type = Column(String)
-    amount = Column(Numeric)
-    currency = Column(String)
-
-
-class PaymentMethods(Base):
-    __tablename__ = 'payment_methods'
-    __table_args__ = {"schema": "cbtools"}
-
-    id = Column(String, primary_key=True)
-    method_type = Column(String)
-    name = Column(String)
-    currency = Column(String)
-    primary_buy = Column(Boolean)
-    primary_sell = Column(Boolean)
-    allow_buy = Column(Boolean)
-    allow_sell = Column(Boolean)
-    allow_deposit = Column(Boolean)
-    allow_withdraw = Column(Boolean)
-    created_at = Column(DateTime(timezone=True))
-    updated_at = Column(DateTime(timezone=True))
-    fiat_account_id = Column(String, ForeignKey('cbtools.accounts.id'))
-    fiat_account = relationship('Accounts', backref=backref('payment_methods', order_by=id))
-
-    document = Column(JSONB)
 
 class Orders(Base):
     __tablename__ = 'orders'
@@ -225,27 +267,12 @@ class Orders(Base):
     transaction_id = Column(String, ForeignKey('cbtools.transactions.id'))
     transaction = relationship('Transactions', uselist=False, backref=backref('order'))
 
+
 # class Refunds(Base):
 #     pass
 #
 # class Mispayments(Base):
 #     pass
-
-
-class Limits(Base):
-    __tablename__ = 'limits'
-    __table_args__ = (UniqueConstraint('payment_method_id', 'limit_type', 'period_in_days',
-                                       name='limits_unique_constraint'),
-                      {"schema": "cbtools"})
-    id = Column(Integer, primary_key=True)
-    payment_method_id = Column(String, ForeignKey('cbtools.payment_methods.id'))
-    payment_method = relationship('PaymentMethods', backref=backref('limits', order_by=id))
-    limit_type = Column(String)
-    period_in_days = Column(Integer)
-    remaining = Column(Numeric)
-    remaining_currency = Column(String)
-    total = Column(Numeric)
-    total_currency = Column(String)
 
 
 class Fills(Base):
